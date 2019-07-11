c70e109152236f3a1058207df1813ad09f4e4fe7
Adrian <40326096+aglowa@users.noreply.github.com>
2019-07-11 23:30:57 +0200

    Update instagram.js

--- instagram.js
+++ instagram.js
@@ -1,4 +1,4 @@
-﻿const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 
 const BASE_URL = 'https://instagram.com/';
 
@@ -14,8 +14,31 @@ initialize: async () => {
 
     instagram.page = await instagram.browser.newPage();
 
-    await instagram.page.goto(BASE_URL, { waitUntil: "networkidle2" });
-}
+   
+    },
+
+    login: async (username,password) => {
+
+        await instagram.page.goto(BASE_URL, { waitUntil: "networkidle2" });
+
+        let loginButton = await instagram.page.$x('//a[contains(text(),"Zaloguj się")]');
+
+    /* Click on the login url button */
+
+        await loginButton[0].click();
+
+        await instagram.page.waitForNavigation({ waitUntil: "networkidle2" });
+
+        await instagram.page.waitFor(2000);
+
+    /* Writing the username and password */
+
+        await instagram.page.type('input[name="username"]',username, { delay: 200 });
+        await instagram.page.type('input[name="password"]',password, { delay: 200 });
+
+        debugger;
+    }
+
 }
 
 module.exports = instagram;
